a0896841b92bd06f9ffb3e5c68d1a924c65b598e
bouakram <kikoua203@gmail.com>
2022-08-23 14:17:05 +0100

    starting with interfaces

---
+++ dist/interfaces.js
@@ -0,0 +1,14 @@
+"use strict";
+let user = {
+    name: "akram",
+    age: 25,
+    country: "algeria"
+};
+let user1 = {
+    name: "aymen",
+    age: 27,
+    country: "algeria"
+};
+console.log(user);
+console.log(user1);
+//# sourceMappingURL=interfaces.js.map
---
+++ src/interfaces.ts
@@ -0,0 +1,22 @@
+// interfaces 
+
+interface user {
+    name : string, 
+    age : number,
+    country : string
+}
+
+let user : user = {
+    name : "akram",
+    age : 25,
+    country : "algeria"
+}
+
+let user1 : user = {
+    name : "aymen",
+    age : 27,
+    country : "algeria"
+}
+
+console.log(user)
+console.log(user1)
