2a0aadf3d2097930a0cc6a5aa5d4e7cb3bc0518e
bouakram <kikoua203@gmail.com>
2022-08-25 15:34:40 +0100

    end of interfaces

--- dist/interfaces.js
+++ dist/interfaces.js
@@ -2,13 +2,36 @@
 let user = {
     name: "akram",
     age: 25,
-    country: "algeria"
+    country: "algeria",
+    sayHello() {
+        return this.name;
+    },
+    getNumber(num) {
+        return num * this.age;
+    }
 };
 let user1 = {
     name: "aymen",
     age: 27,
-    country: "algeria"
+    country: "algeria",
+    sayHello() {
+        return this.name;
+    },
+    getNumber(num) {
+        return num * this.age;
+    }
 };
 console.log(user);
+console.log(user.sayHello());
+console.log(user.getNumber(2));
 console.log(user1);
+console.log(user1.sayHello());
+console.log(user1.getNumber(2));
+let userSetting = {
+    theme: true,
+    font: 'open sans',
+    slider: true,
+    external: true
+};
+console.log(userSetting);
 //# sourceMappingURL=interfaces.js.map
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,22 +1,69 @@
 // interfaces 
 
+// user interface
 interface user {
     name : string, 
     age : number,
-    country : string
+    country : string,
+    sayHello () : string, // method return a string
+    getNumber : (num : number) => number // method return a number
 }
 
 let user : user = {
     name : "akram",
     age : 25,
-    country : "algeria"
+    country : "algeria",
+    sayHello(){
+        return this.name    // return akram in this case
+    },
+    getNumber(num) {
+        return num * this.age   // return the age in this case 25 * number you 'll pase in the param
+    }
 }
 
 let user1 : user = {
     name : "aymen",
     age : 27,
-    country : "algeria"
+    country : "algeria",
+    sayHello(){
+        return this.name
+    },
+    getNumber(num) {
+        return num * this.age
+    }
 }
 
 console.log(user)
+console.log(user.sayHello())
+console.log(user.getNumber(2))
+
 console.log(user1)
+console.log(user1.sayHello())
+console.log(user1.getNumber(2))
+
+// interface reopen and use case
+
+// homepage
+interface Setting {
+    readonly theme : boolean, // can't change the the value
+    font : string
+}
+
+// Articles page
+interface Setting{
+    slider : boolean
+}
+
+// contact page
+interface Setting {
+    external : boolean
+}
+
+let userSetting : Setting = {
+    theme : true,
+    font : 'open sans',
+    slider : true,
+    external : true
+}
+
+console.log(userSetting)
