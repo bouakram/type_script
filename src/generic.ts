744b3b0ab179b7054644f7f6750bd45e24f9fa90
bouakram <kikoua203@gmail.com>
2022-08-31 18:08:42 +0100

    genircs tipes ts end

--- src/generic.ts
+++ src/generic.ts
@@ -4,5 +4,13 @@ function getTipes <GenericType> (val : GenericType) : GenericType {
     return val
 }
 
-console.log(getTipes<number>(15))
-console.log(getTipes<string>('akram'))
+console.log(getTipes<number>(15)) // number tipe
+console.log(getTipes<string>('akram')) // string tipe
+console.log(getTipes<number[]>([1,2,3,4])) // array type
+
+// generic multiple types
+function getTipess <GenericType ,GenericType1 > (val : GenericType , val1 : GenericType1) : string {
+    return `${val} and ${val1}`
+}
+
+console.log(getTipess<string , number>("akram" , 24))
